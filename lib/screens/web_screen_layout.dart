f567bd62b62fe314292da72fd1e2e0af6d218901
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2024-01-08 19:25:54 +0100

    new features

--- lib/screens/web_screen_layout.dart
+++ lib/screens/web_screen_layout.dart
@@ -1,12 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_ui/widget/contact_list.dart';
 
 class WebScreenLayout extends StatelessWidget {
   const WebScreenLayout({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("this is a web screen ")),
+    return Scaffold(
+      body: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: const Column(
+              children: [
+                ContactList(),
+              ],
+            ),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width * 0.75,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('assets/backgroundImage.png'),
+                  fit: BoxFit.cover),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/widget/contact_list.dart
+++ lib/widget/contact_list.dart
@@ -17,28 +17,33 @@ class ContactList extends StatelessWidget {
                     shrinkWrap: true,
                     itemCount: info.length,
                     itemBuilder: (context, index) {
-                      return Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: ListTile(
-                          title: Text(
-                            info[index]['name'].toString(),
-                            style: TextStyle(fontSize: 15),
-                          ),
-                          subtitle: Padding(
-                            padding: EdgeInsets.only(top: 6),
-                            child: Text(
-                              info[index]['message'].toString(),
+                      return InkWell(
+                        onTap: () {},
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: ListTile(
+                            title: Text(
+                              info[index]['name'].toString(),
                               style: TextStyle(fontSize: 15),
                             ),
-                          ),
-                          leading: CircleAvatar(
-                            backgroundImage: NetworkImage(
-                              info[index]['profilePic'].toString(),
+                            subtitle: Padding(
+                              padding: EdgeInsets.only(top: 6),
+                              child: Text(
+                                info[index]['message'].toString(),
+                                style: TextStyle(fontSize: 15),
+                              ),
                             ),
-                          ),
-                          trailing: Text(
-                            info[index]['time'].toString(),
-                            style: TextStyle(fontSize: 15, color: Colors.grey),
+                            leading: CircleAvatar(
+                              radius: 30,
+                              backgroundImage: NetworkImage(
+                                info[index]['profilePic'].toString(),
+                              ),
+                            ),
+                            trailing: Text(
+                              info[index]['time'].toString(),
+                              style:
+                                  TextStyle(fontSize: 15, color: Colors.grey),
+                            ),
                           ),
                         ),
                       );
